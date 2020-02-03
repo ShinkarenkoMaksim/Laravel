235137efdd5c77021dc422456d11707bca0f4ec7
ShinkarenkoMaksim <makc.pst@gmail.com>
2020-02-03 21:16:58 +0300

    Lesson_1

---
+++ Lesson_1/resources/views/Layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<menu type="context">
+    <ul>
+        <li><a href="/">Главная</a></li>
+        <li><a href="/news">Новости</a></li>
+        <li><a href="/articles">Статьи</a></li>
+        <li><a href="/about">О нас</a></li>
+    </ul>
+</menu>
+    @yield('content')
+</body>
+</html>
---
+++ Lesson_1/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/news/', function () {
+    return view('news');
+});
+
+Route::get('/articles/', function () {
+    return view('articles');
+});
+
+Route::get('/about/', function () {
+    return view('about');
+});
